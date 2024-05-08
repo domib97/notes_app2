009e9fe01a6f37a9707e00afbf90e299e6c9d8e1
Dominik Böhm <dominik.boehm@usu.com>
2024-05-08 11:45:03 +0200

    added something

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 
-
 class SearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
