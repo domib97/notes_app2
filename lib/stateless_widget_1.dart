ce7e4b231ed4a1170d9f317f6856b12aea60a6dd
domib97 <dominik.boehm97@gmail.com>
2024-05-17 00:50:38 +0200

    changed something

---
+++ lib/stateless_widget_1.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({Key? key}):super(key: key);
+
+  static const String _title = 'Beispiel';
+
+
+  @override
+  Widget build(BuildContext context){
+    const title = _title,
+    home: MyStatelessWidget(),
+    );
+  }
+}
+
+class MyStatelessWidget extends StatelessWidget{
+  const MyStatelessWidget({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Scaffold-Bsp'),
+        backgroundColor: Colors.cyan
+        bottomNavigationBar: const BottomAppBar(
+          color: Colors.cyanAccent,
+          shape: CircularNotchedRectangle(),
+          child: SizedBox(
+            height: 300,
+            child:  Center(child:Text("bottom_nav_bar")),
+        ),
+      ),
+      body: _buildCardWidget(),
+      ));
+  }
+
+  Widget _buildCardWidget(){
+    return const SizedBox(
+      height: 200,
+      child: Card(
+        child: Center(
+          child: Text('Oberste ard'),
+        ),
+      ),
+    );
+  }
+}
