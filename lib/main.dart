781cce6542ca67bf0d187b66b4dfd87db5159c22
Dominik Böhm <dominik.boehm@usu.com>
2024-05-16 13:00:48 +0200

    added something

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,18 @@ import 'firebase_options.dart';
 import 'sign_up_page.dart';
 import 'sign_in_page.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
+import 'package:supabase_flutter/supabase_flutter.dart' as supabase;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await supabase.Supabase.initialize(
+    // TODO: Replace credentials with your own
+    url: 'https://ktastyizlblacjbjjpmr.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt0YXN0eWl6bGJsYWNqYmpqcG1yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU4MTEzNzQsImV4cCI6MjAzMTM4NzM3NH0.9pBaLiIDmJ-Vpp16Q5ssyJCDcD8nYLJaSpT3qtcLyt4',
+  );
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -45,8 +53,8 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       title: 'NotesApp2',
       theme: ThemeData.light(),
-      home: StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
+      home: StreamBuilder<firebase_auth.User?>(
+        stream: firebase_auth.FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return Scaffold(body: Center(child: CircularProgressIndicator()));
